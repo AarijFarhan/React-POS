4bf49c428f235c64d42cf4e1c29afaaefc4748c5
MuhammadAbsarSiddiqui <absar.sid359@gmail.com>
2025-02-26 03:15:02 +0500

    ch2

--- Frontend/src/components/Starter.jsx
+++ Frontend/src/components/Starter.jsx
@@ -55,21 +55,21 @@ function Starter() {
     <h1 >STARTER</h1>
     <div className='grid  grid-rows-3 sm:grid-rows-6 sm:grid-flow-col  md:grid-rows-6 lg:grid-rows-3 xl:grid-rows-3  w-full '>
         {sta_data.map((input) => (
-    <Card className='flex flex-col  xl:flex-row lg:flex-row md:flex-row justify-center xl:justify-between lg:justify-between md:justify-between rounded-xl m-5 border-0 p-4 ' key={input.title} > 
+    <Card className=' flex flex-col  xl:flex-row lg:flex-row md:flex-row justify-center xl:justify-between lg:justify-between md:justify-between rounded-xl m-5  border-0 p-4 ' key={input.title} > 
        <div className='flex flex-col xl:flex-row lg:flex-row md:flex-row  items-center xl:items-start  '>
     
             <img src={input.images} className='size-fit rounded-full'/> 
             <div className='ml-4' >
             <h1 className='text-lg lg:text-2xl xl:text-2xl xl:text-start lg:text-start md:text-start text-center'>{input.title}</h1>
-        <p className='text-slate-700 text-sm lg:text-lg xl:text-lg xl:text-start lg-text-start md:text-start text-center '>{input.description}</p>
+        <p className='text-slate-700 text-sm lg:text-lg xl:text-xl xl:text-start lg-text-start md:text-start text-center '>{input.description}</p>
             </div>
        </div>
        <div className='flex flex-col xl:flex-row lg:flex-row md:flex-row items-center xl:items-between lg:items-between md:items-between justify-center xl:justify-between lg:justify-between md:justify-between '>
        <NavLink to="/cart">
-          <Button  variant="outline" className="bg-green-500 rounded-2xl border-green-500 text-xs lg:text-sm md:text-sm xl:text-sm size-auto"    >ADD TO CART</Button>
+          <Button  variant="outline" className="bg-green-500 rounded-2xl border-green-500 text-xs  lg:text-sm md:text-sm xl:text-sm size-fit"    >ADD TO CART</Button>
           </NavLink>
           
-          <h5 className='text-lg lg:text-2xl xl:text-2xl text-rose-500 w-5  ml-0 xl:ml-3 md:ml-3 lg:ml-3 m'>{input.price}</h5>
+          <h5 className='text-lg lg:text-2xl xl:text-2xl text-rose-500 w-5 pr-0  ml-0 xl:ml-1 xl:mr-7 md:ml-3 lg:ml-3 m'>{input.price}</h5>
           </div>
     </Card>
 ))
